5e64df61e0593089bea5790b3166f8f9fd98f545
siddhanthgupta <siddhanthgupta@gmail.com>
2015-11-03 11:25:25 +0530

    Add mutexes to access send and receive queues

--- channel.c
+++ channel.c
@@ -31,6 +31,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <semaphore.h>
 
 #define MAX_MESSAGE_SIZE 1024
 
@@ -74,6 +75,7 @@ void create_files() {
     }
     close(fd1);
     close(fd2);
+    srand(1);
 }
 
 void make_send_queue() {
@@ -148,7 +150,157 @@ static int in_cksum(u_short *addr, int len) {
     return (answer);
 }
 
-struct my_message* make_ip_message(int pid, int pid_dest, char* str_message, int msg_type, int msg_seq) {
+/*Semaphore man info:
+ Named semaphores
+ A  named  semaphore  is identified by a name of the form /somename; that is, a null-
+ terminated string of up to NAME_MAX-4 (i.e., 251) characters consisting of  an  ini‐
+ tial slash, followed by one or more characters, none of which are slashes.  Two pro‐
+ cesses can operate on  the  same  named  semaphore  by  passing  the  same  name  to
+ sem_open(3).
+
+ The  sem_open(3)  function  creates a new named semaphore or opens an existing named
+ semaphore.  After the semaphore has  been  opened,  it  can  be  operated  on  using
+ sem_post(3)  and  sem_wait(3).   When a process has finished using the semaphore, it
+ can use sem_close(3) to close the semaphore.  When all processes have finished using
+ the semaphore, it can be removed from the system using sem_unlink(3).
+
+ On  Linux,  named  semaphores  are  created in a virtual filesystem, normally mounted under
+ /dev/shm, with names of the form sem.somename.  (This is the reason  that  semaphore  names
+ are limited to NAME_MAX-4 rather than NAME_MAX characters.)
+
+ */
+
+/*mmap info:
+
+ #include <sys/mman.h>
+
+ void *mmap(void *addr, size_t length, int prot, int flags,
+ int fd, off_t offset);
+
+ mmap()  creates  a  new  mapping  in the virtual address space of the calling process.  The
+ starting address for the new mapping is specified in addr.  The length  argument  specifies
+ the length of the mapping.
+
+ If  addr  is NULL, then the kernel chooses the address at which to create the mapping; this
+ is the most portable method of creating a new mapping.
+
+ The prot argument describes the desired memory protection of the mapping (and must not con‐
+ flict  with the open mode of the file).  It is either PROT_NONE or the bitwise OR of one or
+ more of the following flags:
+
+ PROT_EXEC  Pages may be executed.
+
+ PROT_READ  Pages may be read.
+
+ PROT_WRITE Pages may be written.
+
+ PROT_NONE  Pages may not be accessed.
+
+ The flags argument determines whether updates to the mapping are visible to other processes
+ mapping  the  same  region, and whether updates are carried through to the underlying file.
+ This behavior is determined by including exactly one of the following values in flags:
+
+ MAP_SHARED Share this mapping.  Updates to the mapping are visible to other processes  that
+ map this file, and are carried through to the underlying file.  The file may not
+ actually be updated until msync(2) or munmap() is called.
+
+ MAP_ANONYMOUS
+ The mapping is not backed by any file; its contents are initialized to zero.  The fd
+ and  offset arguments are ignored; however, some implementations require fd to be -1
+ if MAP_ANONYMOUS (or MAP_ANON) is specified, and portable applications should ensure
+ this.  The use of MAP_ANONYMOUS in conjunction with MAP_SHARED is supported on Linux
+ only since kernel 2.4.
+ */
+
+sem_t* mutex_send;                    //Semaphore to access the buffer
+sem_t* mutex_receive;                      //Holds the number of full buffers (intiialised to 0)
+
+/*     sem_wait() decrements (locks) the semaphore pointed to by sem.  If the semaphore's value is
+ greater than zero, then the decrement proceeds, and the function returns, immediately.   If
+ the  semaphore  currently  has the value zero, then the call blocks until either it becomes
+ possible to perform the decrement (i.e., the semaphore value rises above zero), or a signal
+ handler interrupts the call.
+
+ All of these functions return 0 on success; on error, the value of the  semaphore  is  left
+ unchanged, -1 is returned, and errno is set to indicate the error.
+
+ ERRORS
+ EINTR  The call was interrupted by a signal handler; see signal(7).
+
+ */
+//Function encapsulates error handling for the sem_wait
+void semaphoreWait(sem_t* semaphore) {
+    int x;
+    fprintf(stderr, "Status Report: semaphoreWait : Semaphore wait sent from process %ld.\n",
+            (long) getpid());
+    while ((x = sem_wait(semaphore)) == -1 && errno == EINTR)    //Restart if interrupted by handler
+        continue;
+    if (x == -1)                                         //Some error happened. We abort the program
+            {
+        fprintf(stderr, "Error: semaphoreWait : Semaphore failed to lock (wait)\n");
+        abort();
+    }
+    fprintf(stderr, "Status Report: semaphoreWait : Process %ld ain't waitin' no more.\n",
+            (long) getpid());
+}
+
+/*
+ sem_post(3)
+ SYNOPSIS
+ #include <semaphore.h>
+
+ int sem_post(sem_t *sem);
+
+ Link with -pthread.
+
+ DESCRIPTION
+ sem_post()  increments  (unlocks) the semaphore pointed to by sem.  If the semaphore's value consequently becomes
+ greater than zero, then another process or thread blocked in a sem_wait(3) call will be woken up and  proceed  to
+ lock the semaphore.
+
+ RETURN VALUE
+ sem_post()  returns  0  on  success;  on error, the value of the semaphore is left unchanged, -1 is returned, and
+ errno is set to indicate the error.
+ */
+
+//Function encapsulates the sem_post error handling part
+void semaphoreSignal(sem_t* semaphore) {
+    int x;
+    x = sem_post(semaphore);
+    fprintf(stderr, "Status Report: semaphoreSignal : Semaphore signal sent from process %ld.\n",
+            (long) getpid());
+    if (x == -1) {
+        fprintf(stderr, "Error: semaphoreSignal : Semaphore failed to unlock (signal)\n");
+        abort();
+    }
+}
+
+//Initialises the semaphores and shared variables
+int initializeSemaphores() {
+    int i;
+    char str[1024];
+    sprintf(str, "MUTEX SEND QUEUE");
+    sem_unlink(str);
+    //printf("%s\n",str);
+    if ((mutex_send = sem_open(str, O_CREAT, S_IRUSR | S_IWUSR, 1)) == SEM_FAILED) {
+        fprintf(stderr, "Error: semaphoreInitialization : Semaphore %s failed to initialise\n",
+                str);
+        abort();
+    }
+
+    sprintf(str, "MUTEX RECEIVE QUEUE");
+    sem_unlink(str);
+    //printf("%s\n",str);
+    if ((mutex_receive = sem_open(str, O_CREAT, S_IRUSR | S_IWUSR, 1)) == SEM_FAILED) {
+        fprintf(stderr, "Error: semaphoreInitialization : Semaphore %s failed to initialise\n",
+                str);
+        abort();
+    }
+    return 1;
+}
+
+struct my_message* make_ip_message(int pid, int pid_dest, char* str_message, int msg_type,
+        int msg_seq) {
     struct my_message* message = (struct my_message*) malloc(sizeof(struct my_message));
     message->mtype = (long) pid;
     strcpy((message->data).message, str_message);
@@ -162,47 +314,68 @@ struct my_message* make_ip_message(int pid, int pid_dest, char* str_message, int
 }
 
 void send_message_to_send_queue(struct my_message* message) {
+    semaphoreWait(mutex_send);
     if (msgsnd(send_queue_id, message, sizeof(struct my_message) - sizeof(long), 0) == -1) {
-        fprintf(stderr, "Channel error: Unable to send to send queue.\n");
+        printf("Channel error: Unable to send to send queue.\n");
+        semaphoreSignal(mutex_send);
         exit(1);
     }
+    semaphoreSignal(mutex_send);
 }
 
 struct my_message* receive_message_from_receive_queue(int pid) {
     struct my_message* message = (struct my_message*) malloc(sizeof(struct my_message));
-    int status = msgrcv(receive_queue_id, message, sizeof(struct my_message) - sizeof(long), (long) pid, IPC_NOWAIT);
+    semaphoreWait(mutex_receive);
+    int status = msgrcv(receive_queue_id, message, sizeof(struct my_message) - sizeof(long),
+            (long) pid, IPC_NOWAIT);
     if (status == -1) {
-        if (errno == ENOMSG)
+        if (errno == ENOMSG) {
+            semaphoreSignal(mutex_receive);
             return NULL;
+        }
         fprintf(stderr, "Channel error: Unable to receive from receive queue.\n");
+        semaphoreSignal(mutex_receive);
         exit(1);
     }
+    semaphoreSignal(mutex_receive);
     return message;
 }
 
 void send_message_to_receive_queue(struct my_message* message) {
+    semaphoreWait(mutex_receive);
     if (msgsnd(receive_queue_id, message, sizeof(struct my_message) - sizeof(long), 0) == -1) {
         fprintf(stderr, "Channel error: Unable to send to receive queue.\n");
+        semaphoreSignal(mutex_receive);
         exit(1);
     }
+    semaphoreSignal(mutex_receive);
 }
 
 struct my_message* receive_message_from_send_queue(int pid) {
+    semaphoreWait(mutex_send);
     struct my_message* message = (struct my_message*) malloc(sizeof(struct my_message));
-    if (msgrcv(send_queue_id, message, sizeof(struct my_message) - sizeof(long), (long) pid, 0) == -1) {
+    int status = msgrcv(send_queue_id, message, sizeof(struct my_message) - sizeof(long),
+            (long) pid, IPC_NOWAIT);
+    if (status == -1) {
+        if (errno == ENOMSG) {
+            semaphoreSignal(mutex_send);
+            return NULL;
+        }
         fprintf(stderr, "Channel error: Unable to receive from send queue.\n");
+        semaphoreSignal(mutex_send);
         exit(1);
     }
+    semaphoreSignal(mutex_send);
     return message;
 }
 
 int transfer_messages_between_queues() {
     struct my_message** msg_arr = (struct my_message**) malloc(sizeof(struct my_message*) * 100);
+
     int c = 0, status = 0, i = 0;
     do {
-        struct my_message* message = (struct my_message*) malloc(sizeof(struct my_message));
-        status = msgrcv(send_queue_id, message, sizeof(struct my_message) - sizeof(long), 0, IPC_NOWAIT);
-        if (status == -1) {
+        struct my_message* message = receive_message_from_send_queue(0);
+        if (message == NULL) {
             break;
         }
         msg_arr[c] = message;
@@ -210,7 +383,11 @@ int transfer_messages_between_queues() {
     } while (status != -1);
     for (i = c - 1; i >= 0; i--) {
         msg_arr[i]->mtype = (msg_arr[i]->data).dest_pid;
-        send_message_to_receive_queue(msg_arr[i]);
+        int r = rand() % 100;
+        if (r < 85)
+//        if(msg_arr[i]->data.msg_type == DATA)
+//            printf("CHANNEL : Transferring message %s\n",msg_arr[i]->data.message);
+            send_message_to_receive_queue(msg_arr[i]);
     }
     return c;
 }
@@ -218,12 +395,19 @@ int transfer_messages_between_queues() {
 void destroy_channel() {
     msgctl(send_queue_id, IPC_RMID, NULL);
     msgctl(receive_queue_id, IPC_RMID, NULL);
+    char str[150];
+    sprintf(str, "MUTEX SEND QUEUE");
+    sem_unlink(str);
+
+    sprintf(str, "MUTEX RECEIVE QUEUE");
+    sem_unlink(str);
 }
 
 int main() {
     initialise_channel();
     destroy_channel();
     initialise_channel();
+    initializeSemaphores();
     pid_t pid_receiver = fork();
     if (pid_receiver < 0) {
         fprintf(stderr, "Channel: Error: Unable to fork receiver process.\n");
@@ -232,34 +416,26 @@ int main() {
         // Child receiver Process
         pid_t pid = getpid();
         printf("Receiver process is %d\n", (int) pid);
-        int c = 0, attempts = 0, expected_seq = 0;
-        while (c < 5 && attempts < 20) {
+        int c = 0, attempts = 0;
+        char dummy_str[2] = "\0";
+        while (attempts < 50) {
+            sleep(3);
             attempts++;
             struct my_message* message = receive_message_from_receive_queue(pid);
             if (message != NULL) {
-                c++;
-                printf("RECEIVER PROCESS %d receives message number %d: %s\n", (int) pid, c,
-                        (message->data).message);
+                printf("RECEIVER %d : Receives message: %s\n", (int) pid, (message->data).message);
                 int obtained_seq = (message->data).msg_seq;
-                if (obtained_seq == expected_seq) {
-                    // The sequence number is what was expected
-                    // In this case, we send an acknowledgement with the updated expected number
-                    expected_seq = (expected_seq + 1) % 2;
-                    printf("Expected sequence number received\n");
-                }
-                // If the sequence number received was as expected, expected sequence number has
-                // been updated. Otherwise, it sends an ACK with the same sequence number as last
-                // time
-                struct my_message* ret_msg = make_ip_message(pid, (message->data).src_pid, (message->data).message, ACK,
-                        expected_seq);
+                struct my_message* ret_msg = make_ip_message(pid, (message->data).src_pid,
+                        dummy_str, ACK, obtained_seq);
                 send_message_to_send_queue(ret_msg);
+            } else {
+                printf("RECEIVER %d : No message received\n", (int) pid);
             }
-            sleep(3);
         }
         if (attempts == 20) {
-            printf("Receiver got tired of waiting.\n");
+            printf("RECEIVER %d : Receiver got tired of waiting.\n", (int) pid);
         }
-        printf("Receiver got %d messages\n", c);
+        printf("RECEIVER %d : Exiting.\n", (int) pid);
     } else {
         // Parent Process
         pid_t pid_sender = fork();
@@ -268,42 +444,47 @@ int main() {
             exit(1);
         } else if (pid_sender == 0) {
             // Child sender process
-            printf("according to sender, receiver process is %d\n", pid_receiver);
             pid_t pid = getpid();
-            int i;
-            for (i = 0; i < 5; i++) {
+            printf("SENDER %d : Receiver process is %d\n", (int) pid, pid_receiver);
+            int i, quit_count = 0;
+            for (i = 0; i < 10 && quit_count < 10; i++) {
                 char str[MAX_MESSAGE_SIZE];
-                sprintf(str, "Hello, This is message %d", i + 1);
+                sprintf(str, "Hello, This is message %d", i);
                 struct my_message* message = make_ip_message(pid, pid_receiver, str, DATA, i % 2);
                 send_message_to_send_queue(message);
-                //printf("Sender sent message number %d\n", i + 1);
+                printf("SENDER %d : Sender sent message %s\n", (int) pid, message->data.message);
                 sleep(6);
                 struct my_message* ack_msg = receive_message_from_receive_queue(pid);
-                if (ack_msg == NULL || (ack_msg->data).msg_seq != (i+1)%2) {
-                    // did not receive acknowledgement
+                if (ack_msg == NULL) {
+                    // did not receive acknowledgment
                     // Resends the message again
-                    printf("Invalid seq or no ack\n");
+                    printf("SENDER %d: Did not receive ACK\n", (int) pid);
+                    quit_count++;
+                    if (quit_count == 10)
+                        printf("SENDER %d: Too many quits. Exiting.\n", (int) pid);
                     i--;
                 } else {
-                    printf("Received acknowledgement saying send message with seq %d\n",
-                            (ack_msg->data).msg_seq);
+                    printf(
+                            "SENDER %d: Received acknowledgement saying received message with seq %d\n",
+                            (int) pid, (ack_msg->data).msg_seq);
+                    quit_count = 0;
                 }
             }
+            printf("SENDER %d : Exiting.\n", (int) pid);
 
         } else {
             // parent process
             pid_t pid;
             int i;
-            for (i = 0; i < 15; i++) {
-                sleep(2);
+            for (i = 0; i < 1000; i++) {
+                sleep(1);
+                //       printf("CHANNEL : Attempting Transfer\n");
                 int x = transfer_messages_between_queues();
-               // printf("%d messages transferred by channel\n", x);
+                //       printf("CHANNEL : Transfer Complete. %d Messages transferred\n", x);
+                // printf("%d messages transferred by channel\n", x);
             }
-            while ((pid = waitpid(-1, NULL, WNOHANG)) != 0) {
-                if (errno == ECHILD) {
-                    break;
-                }
-            }
+            while (wait(NULL) != -1)
+                ;
             destroy_channel();
         }
     }
